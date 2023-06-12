f165e751bdd0ff7958909390d9645b4eb377e102
flarnrules <breifenberg11@gmail.com>
2023-06-12 16:57:20 -0500

    print webpage, webpage crawler code

--- 1-python-for-everybody/lessons/network-programming/print_webpage.py
+++ 1-python-for-everybody/lessons/network-programming/print_webpage.py
@@ -4,3 +4,6 @@ fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
 for line in fhand:
     print(line.decode().strip())
 
+# this is essentially the starting part to a web crawler
+# it reads a web page
+# we now need to learn how to parse this html text
