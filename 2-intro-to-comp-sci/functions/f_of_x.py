96fa7f976d7de976a2e295cc641820f1bdec12d8
flarnrules <breifenberg11@gmail.com>
2023-07-27 12:23:16 -0500

    some functions

---
+++ 2-intro-to-comp-sci/functions/f_of_x.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 11:46:37 2023
+
+@author: flarnrules
+"""
+
+def f ( x ):
+	x = x + 1
+	print('in f(x): x =', x)
+	return x
+
+x = 3
+z = f( x )
